5b63ad9913869f98858ad9ab057f580cb715b4e6
santiago goez <128654665+Santy0240@users.noreply.github.com>
2024-05-21 20:15:23 -0500

    sisven/vue

---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue';
+import Categories from '../views/categories';
+import Customers from '../views/customers';
+import Details from '../views/details';
+import Invoices from '../views/invoices';
+import Products from '../views/products';
+import Pay_mode from '../views/pay_mode';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: Categories
+  },
+  {
+    path: '/customers',
+    name: 'Customers',
+    component: Customers
+  },
+  {
+    path: '/details',
+    name: 'Details',
+    component: Details
+  },
+  {
+    path: '/invoices',
+    name: 'Invoices',
+    component: Invoices
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/pay_mode',
+    name: 'Pay_mode',
+    component: Pay_mode
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
